2c343415f27a998ddf3a41684f3d924e511c65f5
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 21:55:19 +0200

    Add Roles manipulation for User

--- src/ProductManager/ProductManager.Core/Domain/User.cs
+++ src/ProductManager/ProductManager.Core/Domain/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using ProductManager.Core.Exceptions;
@@ -14,6 +15,8 @@ namespace ProductManager.Core.Domain
         private readonly Regex _emailRegex = new Regex(@"^[\w.-]+@(?=[a-z\d][^.]*\.)[a-z\d.-]*[^.]$");
         public string Password { get; protected set; }
         private readonly Regex _passwordRegex = new Regex(@"^[a-zA-Z0-9]");
+        protected List<Role> _roles = new List<Role>();
+        public IEnumerable<Role> Roles => _roles.AsEnumerable();
 
         protected User()
         {
@@ -32,6 +35,20 @@ namespace ProductManager.Core.Domain
             string email, string password)
         {
             Id = Guid.NewGuid();
+            SetName(name);
+            SetEmail(email);
+            SetPassword(password);
+        }
+
+        public User(Guid id, string name,
+            string email, string password, params Role[] roles)
+        {
+            Id = id;
+            //SetRole(role);
+            foreach (var role in roles)
+            {
+                AddRole(role);
+            }
             SetName(name);
             SetEmail(email);
             SetPassword(password);
@@ -62,6 +79,26 @@ namespace ProductManager.Core.Domain
                 throw new InvalidPasswordException("User password should contain only alphanumeric characters.");
             }
             Password = password;
+        }
+
+        public void AddRole(Role role)
+        {
+            if (_roles.Any(r => r.Name == role.Name))
+            {
+                throw new DuplicateRoleException("User already has specified role.");
+            }
+
+            _roles.Add(role);
+        }
+
+        public void RemoveRole(Role role)
+        {
+            if (_roles.All(r => r.Name != role.Name))
+            {
+                throw new RoleNotFoundException("User doesn't have specified role.");
+            }
+
+            _roles.Remove(role);
         }
     }
 
