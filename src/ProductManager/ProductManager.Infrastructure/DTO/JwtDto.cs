e736e469b5d00cf5eb20584fded1ede1f1fc3170
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:46:06 +0200

    Add JwtDto

---
+++ src/ProductManager/ProductManager.Infrastructure/DTO/JwtDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager.Infrastructure.DTO
+{
+    public class JwtDto
+    {
+        public string Token { get; set; }
+        public long Expires { get; set; }
+    }
+}
