dd1b5ac4236295cac01142c02f8c152817944257
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 21:37:19 +0200

    Add user validation tests

---
+++ src/ProductManager/ProductManager.UnitTests/UserTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace ProductManager.UnitTests
+{
+    public class UserTests
+    {
+
+        [Theory]
+        [InlineData("test", "test@email.com", "pass")]
+        public void CreatingNewUser_WithValidProperties_ShouldNotThrowAnyExceptions(string name, string email, string password)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                var user = new User(name, email, password);
+            };
+
+            action.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("", "email@email.com", "pass")]
+        [InlineData(" ", "email@email.com", "pass")]
+        [InlineData("test ", "email@email.com", "pass")]
+        [InlineData("te st", "email@email.com", "pass")]
+        [InlineData(" test", "email@email.com", "pass")]
+        [InlineData("/", "email@email.com", "pass")]
+        [InlineData("/test", "email@email.com", "pass")]
+        [InlineData("test*", "email@email.com", "pass")]
+        public void CreatingNewUser_WithInvalidName_ThrowsInvalidUsernameException(string name, string email, string password)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                var user = new User(name, email, password);
+            };
+
+            // Act/Assert
+            action.Should().Throw<InvalidUsernameException>();
+        }
+
+        [Theory]
+        [InlineData("test", "", "pass")]
+        [InlineData("test", " ", "pass")]
+        [InlineData("test", "email @email.com", "pass")]
+        [InlineData("test", "email/ @email.com", "pass")]
+        [InlineData("test", "email/* @email.com", "pass")]
+        public void CreatingNewUser_WithInvalidEmail_ThrowsInvalidEmailException(string name, string email, string password)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                var user = new User(name, email, password);
+            };
+
+            // Act/Assert
+            action.Should().Throw<InvalidEmailException>();
+        }
+
+        [Theory]
+        [InlineData("test", "email@email.com", "")]
+        [InlineData("test", "email@email.com", " ")]
+        [InlineData("test", "email@email.com", "*")]
+        [InlineData("test", "email@email.com", "/")]
+        public void CreatingNewUser_WithInvalidPassword_ThrowsInvalidPasswordException(string name, string email, string password)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                var user = new User(name, email, password);
+            };
+
+            // Act/Assert
+            action.Should().Throw<InvalidPasswordException>();
+        }
+    }
+}
