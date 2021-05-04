49ae3e172fd829d0acb6230d540f0eabd517b25a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:31:21 +0200

    Add InMemoryUserRepository

---
+++ src/ProductManager/ProductManager.Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Repositories;
+
+namespace ProductManager.Infrastructure.Repositories
+{
+    public class InMemoryUserRepository : IUserRepository
+    {
+        private static readonly ISet<Role> _roles = new HashSet<Role>
+        {
+            new Role(Guid.NewGuid(), "user"),
+            new Role(Guid.NewGuid(), "admin")
+        };
+
+        private static readonly ISet<User> _users = new HashSet<User>
+        {
+            new User(Guid.NewGuid(), "User", "user@user.com", "secret", _roles.SingleOrDefault(r => r.Name == "user")),
+            new User(Guid.NewGuid(), "Admin", "admin@admin.com", "secret", _roles.ToArray())
+        };
+
+        public async Task<Role> GetRoleAsync(Guid id)
+            => await Task.FromResult(_roles.SingleOrDefault(x => x.Id == id));
+
+        public async Task<Role> GetRoleAsync(string name)
+            => await Task.FromResult(_roles.SingleOrDefault(x => 
+                string.Equals(x.Name, name, StringComparison.InvariantCultureIgnoreCase)));
+
+        public Task AddAsync(Role role)
+        {
+            _roles.Add(role);
+            return Task.CompletedTask;
+        }
+
+        public async Task<User> GetUserAsync(Guid id)
+            => await Task.FromResult(_users.SingleOrDefault(x => x.Id == id));
+
+        public async Task<User> GetUserAsync(string email)
+            => await Task.FromResult(_users.SingleOrDefault(x =>
+                string.Equals(x.Email, email, StringComparison.InvariantCultureIgnoreCase)));
+
+        public async Task AddAsync(User user)
+        {
+            _users.Add(user);
+            await Task.CompletedTask;
+        }
+    }
+}
