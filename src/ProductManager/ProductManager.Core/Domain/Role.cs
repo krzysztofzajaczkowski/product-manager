3256df08200e470dc279dd5c7a4a943d6b5f6deb
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 21:54:55 +0200

    Add Role class

---
+++ src/ProductManager/ProductManager.Core/Domain/Role.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using ProductManager.Core.Exceptions;
+
+namespace ProductManager.Core.Domain
+{
+    public class Role : Entity
+    {
+        public string Name { get; protected set; }
+        private readonly Regex _nameRegex = new Regex(@"^[a-zA-Z0-9]+$");
+
+        protected Role()
+        {
+        }
+
+        public Role(Guid id, string name)
+        {
+            Id = id;
+            SetName(name);
+        }
+
+        public Role(string name)
+        {
+            SetName(name);
+        }
+
+        private void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name) || !_nameRegex.IsMatch(name))
+            {
+                throw new InvalidRoleNameException("Role name should contain only alphanumeric characters.");
+            }
+            Name = name;
+        }
+    }
+}
