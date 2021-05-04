9ce9e8da7c844ed535df7f9b291d9db262380e26
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:46:21 +0200

    Add DateTime extension for Timestamp in seconds

---
+++ src/ProductManager/ProductManager.Infrastructure/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager.Infrastructure.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToTimestamp(this DateTime dateTime)
+        {
+            var epoch = new DateTime(1970, 1, 1);
+            var time = dateTime.Subtract(epoch);
+
+            return (long) time.TotalSeconds;
+        }
+    }
+}
