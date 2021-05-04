e55f4d8ce221262415695c6de652abeee3f86100
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:46:44 +0200

    Add JwtHandler

---
+++ src/ProductManager/ProductManager.Infrastructure/Services/JwtHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ProductManager.Infrastructure.DTO;
+using ProductManager.Infrastructure.Extensions;
+using ProductManager.Infrastructure.Settings;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace ProductManager.Infrastructure.Services
+{
+    public class JwtHandler : IJwtHandler
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtHandler(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public JwtDto CreateToken(Guid userId, string role)
+        {
+            var now = DateTime.UtcNow;
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, userId.ToString()),
+                new Claim(ClaimTypes.Role, role),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, now.ToTimestamp().ToString()),
+            };
+
+            var expires = now.AddMinutes(_jwtSettings.ExpiryMinutes);
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key)),
+                SecurityAlgorithms.HmacSha256);
+            var jwt = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                claims: claims,
+                notBefore: now,
+                expires: expires,
+                signingCredentials: signingCredentials
+            );
+            var token = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtDto
+            {
+                Token = token,
+                Expires = expires.ToTimestamp()
+            };
+        }
+    }
+}
