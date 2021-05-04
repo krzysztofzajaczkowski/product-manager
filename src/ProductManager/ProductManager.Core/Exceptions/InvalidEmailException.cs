bd8270684d4539b02977d240372e5088db87194e
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 21:38:23 +0200

    Add User domain exceptions

---
+++ src/ProductManager/ProductManager.Core/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager.Core.Exceptions
+{
+    public class InvalidEmailException : Exception
+    {
+        public InvalidEmailException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ src/ProductManager/ProductManager.Core/Exceptions/InvalidUsernameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager.Core.Exceptions
+{
+    public class InvalidUsernameException : Exception
+    {
+        public InvalidUsernameException(string message) : base(message)
+        {
+
+        }
+    }
+}
