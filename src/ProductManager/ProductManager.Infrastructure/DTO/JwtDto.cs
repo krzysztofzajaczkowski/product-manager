a8f24d1d2119cc6b9bf4b4ffd1961dd34664aa77
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:56:37 +0200

    Add Role property to JwtDto

--- src/ProductManager/ProductManager.Infrastructure/DTO/JwtDto.cs
+++ src/ProductManager/ProductManager.Infrastructure/DTO/JwtDto.cs
@@ -7,6 +7,7 @@ namespace ProductManager.Infrastructure.DTO
     public class JwtDto
     {
         public string Token { get; set; }
+        public string Role { get; set; }
         public long Expires { get; set; }
     }
 }
