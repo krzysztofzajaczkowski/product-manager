b40f0cef1d0684c70986d5805403ab02699c6338
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 21:54:18 +0200

    Add unit tests for Role and Role manipulation in User

---
+++ src/ProductManager/ProductManager.UnitTests/RoleTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Exceptions;
+using Xunit;
+
+namespace ProductManager.UnitTests
+{
+    public class RoleTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("*")]
+        public void CreatingNewRole_WithInvalidName_ShouldThrowInvalidRoleNameException(string name)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                var role = new Role(name);
+            };
+
+            // Act/Assert
+            action.Should().Throw<InvalidRoleNameException>();
+        }
+
+        [Fact]
+        public void CreatingNewRole_WithValidName_ShouldNotThrowAnyException()
+        {
+            // Arrange
+            var name = "testRole";
+            Action action = () =>
+            {
+                var role = new Role(name);
+            };
+
+            // Act/Assert
+            action.Should().NotThrow();
+        }
+
+    }
+}
