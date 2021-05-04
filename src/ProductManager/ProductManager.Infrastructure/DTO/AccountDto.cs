d8c9e2f0a882660f9535e3cb7fc84ee7cb1fe1a8
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:57:01 +0200

    Add AccountDto

---
+++ src/ProductManager/ProductManager.Infrastructure/DTO/AccountDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Infrastructure.DTO
+{
+    public class AccountDto
+    {
+        public Guid Id { get; set; }
+        public List<string> Roles { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
