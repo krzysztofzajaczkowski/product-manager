abc2e43dc419ebfdcd01a7dda7dc2ebeed2f2c1e
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 21:37:40 +0200

    Add base Entity class

---
+++ src/ProductManager/ProductManager.Core/Domain/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
