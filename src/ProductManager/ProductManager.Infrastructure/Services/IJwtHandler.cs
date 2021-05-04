d8445be3b59dca02a045681683f38b25798b6a5c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:46:44 +0200

    Add JwtHandler

---
+++ src/ProductManager/ProductManager.Infrastructure/Services/IJwtHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProductManager.Infrastructure.DTO;
+
+namespace ProductManager.Infrastructure.Services
+{
+    public interface IJwtHandler
+    {
+        JwtDto CreateToken(Guid userId, string role);
+    }
+}
