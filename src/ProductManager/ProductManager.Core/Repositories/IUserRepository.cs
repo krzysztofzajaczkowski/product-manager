b4ae3eacff423770ffc23a00c8bb49a282efb60c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-04 22:12:01 +0200

    Add IUserRepository interface

---
+++ src/ProductManager/ProductManager.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Core.Domain;
+
+namespace ProductManager.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserAsync(Guid id);
+        Task<User> GetUserAsync(string email);
+        Task AddAsync(User user);
+        Task<Role> GetRoleAsync(Guid id);
+        Task<Role> GetRoleAsync(string name);
+        Task AddAsync(Role role);
+    }
+}
