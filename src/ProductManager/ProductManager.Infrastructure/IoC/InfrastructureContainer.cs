30f87cd1d5cca9911b557c20d804d336e7612ba3
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-06 23:06:48 +0200

    Add InfrastructureContainer class with AddInfrastructure method

---
+++ src/ProductManager/ProductManager.Infrastructure/IoC/InfrastructureContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductManager.Core.Repositories;
+using ProductManager.Infrastructure.Repositories;
+using ProductManager.Infrastructure.Services;
+using ProductManager.Infrastructure.Settings;
+
+namespace ProductManager.Infrastructure.IoC
+{
+    public static class InfrastructureContainer
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<JwtSettings>(config.GetSection("jwt"));
+
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddTransient<IJwtHandler, JwtHandler>();
+
+            return services;
+        }
+    }
+}
