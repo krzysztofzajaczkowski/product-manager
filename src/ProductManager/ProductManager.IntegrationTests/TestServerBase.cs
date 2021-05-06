aa3fd6b281305feef4f512ee0560e76a85c9282c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-06 23:04:31 +0200

    Add TestServerBase in IntegrationTests

---
+++ src/ProductManager/ProductManager.IntegrationTests/TestServerBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using ProductManager.Infrastructure.Settings;
+using ProductManager.Web;
+
+namespace ProductManager.IntegrationTests
+{
+    public class TestServerBase
+    {
+        protected JwtSettings _jwtSettings = new JwtSettings
+        {
+            ExpiryMinutes = 1,
+            Issuer = "testIssuer",
+            Key = "secret"
+        };
+        protected TestServer BuildTestServer()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureServices(services =>
+                {
+                    services.Configure<JwtSettings>(x =>
+                    {
+                        //x = _jwtSettings;
+                        x.ExpiryMinutes = _jwtSettings.ExpiryMinutes;
+                        x.Issuer = _jwtSettings.Issuer;
+                        x.Key = _jwtSettings.Key;
+                    });
+                });
+            return new TestServer(webHostBuilder);
+        }
+    }
+}
