24a6a154a5b21c2b1506b4b8d5bdcdc029e4aedb
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-06 22:45:20 +0200

    Add empty UserService class with interface and unit tests

---
+++ src/ProductManager/ProductManager.Infrastructure/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Exceptions;
+using ProductManager.Core.Repositories;
+using ProductManager.Infrastructure.DTO;
+using ProductManager.Infrastructure.Extensions;
+
+namespace ProductManager.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtHandler _jwtHandler;
+
+        public UserService(IUserRepository userRepository, IJwtHandler jwtHandler)
+        {
+            _userRepository = userRepository;
+            _jwtHandler = jwtHandler;
+        }
+
+        public Task<AccountDto> GetAccountAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterAsync(Guid userId, string email, string name, string password, string roleName = "user")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JwtDto> LoginAsync(string email, string password, string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
