983ff17b033f5577d0e3b1006cb1d59d19935a42
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-07 18:14:55 +0200

    Add GetRoleOrFailAsync extensions methods

---
+++ src/ProductManager/ProductManager.Infrastructure/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Exceptions;
+using ProductManager.Core.Repositories;
+
+namespace ProductManager.Infrastructure.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static async Task<User> GetOrFailAsync(this IUserRepository repository, Guid id)
+        {
+            var user = await repository.GetUserAsync(id);
+            if (user == null)
+            {
+                throw new UserNotFoundException($"User with id: '{id}' does not exist.");
+            }
+
+            return user;
+        }
+
+        public static async Task<Role> GetRoleOrFailAsync(this IUserRepository repository, Guid id)
+        {
+            var role = await repository.GetRoleAsync(id);
+            if (role == null)
+            {
+                throw new RoleNotFoundException("Specified role does not exist.");
+            }
+
+            return role;
+        }
+
+        public static async Task<Role> GetRoleOrFailAsync(this IUserRepository repository, string roleName)
+        {
+            var role = await repository.GetRoleAsync(roleName);
+            if (role == null)
+            {
+                throw new RoleNotFoundException("Specified role does not exist.");
+            }
+
+            return role;
+        }
+    }
+}
