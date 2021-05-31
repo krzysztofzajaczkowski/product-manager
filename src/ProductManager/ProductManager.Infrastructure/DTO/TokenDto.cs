b797f0e21199fe6f68c039080af34f677bbce00d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-31 19:25:41 +0200

    Add TokenDto

---
+++ src/ProductManager/ProductManager.Infrastructure/DTO/TokenDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Infrastructure.DTO
+{
+    public class TokenDto
+    {
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public long Expires { get; set; }
+    }
+}
