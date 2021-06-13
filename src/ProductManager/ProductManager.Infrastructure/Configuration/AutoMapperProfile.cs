74cbbcdc01918b5e5091e8e42b312f2aa8050a07
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-13 22:31:48 +0200

    Create mapping between Product and ProductDto

--- src/ProductManager/ProductManager.Infrastructure/Configuration/AutoMapperProfile.cs
+++ src/ProductManager/ProductManager.Infrastructure/Configuration/AutoMapperProfile.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using ProductManager.Core.Domain;
+using ProductManager.Infrastructure.DTO;
 
 namespace ProductManager.Infrastructure.Configuration
 {
@@ -17,6 +18,33 @@ namespace ProductManager.Infrastructure.Configuration
             CreateMap<WarehouseProduct, WarehouseProduct>();
             CreateMap<SalesProduct, SalesProduct>();
             CreateMap<CatalogProduct, CatalogProduct>();
+
+            CreateMap<Product, ProductDto>()
+                .ForMember(x => x.CatalogId, x => x.MapFrom(y => y.CatalogProduct.Id))
+                .ForMember(x => x.Sku, x => x.MapFrom(y => y.CatalogProduct.Sku))
+                .ForMember(x => x.Name, x => x.MapFrom(y => y.CatalogProduct.Name))
+                .ForMember(x => x.Description, x => x.MapFrom(y => y.CatalogProduct.Description))
+                .ForMember(x => x.WarehouseId, x => x.MapFrom(y => y.WarehouseProduct.Id))
+                .ForMember(x => x.Stock, x => x.MapFrom(y => y.WarehouseProduct.Stock))
+                .ForMember(x => x.Weight, x => x.MapFrom(y => y.WarehouseProduct.Weight))
+                .ForMember(x => x.SalesId, x => x.MapFrom(y => y.SalesProduct.Id))
+                .ForMember(x => x.NetPrice, x => x.MapFrom(y => y.SalesProduct.NetPrice))
+                .ForMember(x => x.Cost, x => x.MapFrom(y => y.SalesProduct.Cost))
+                .ForMember(x => x.TaxPercentage, x => x.MapFrom(y => y.SalesProduct.TaxPercentage));
+
+            CreateMap<ProductDto, Product>()
+                .ForMember(x => x.CatalogProduct,
+                    x => x.MapFrom(y =>
+                        new CatalogProduct(y.CatalogId != Guid.Empty ? y.CatalogId : Guid.NewGuid(),
+                            y.Sku, y.Name, y.Description)))
+                .ForMember(x => x.SalesProduct,
+                    x => x.MapFrom(y =>
+                        new SalesProduct(y.SalesId != Guid.Empty ? y.SalesId : Guid.NewGuid(),
+                            y.Sku, y.Cost, y.TaxPercentage, y.NetPrice)))
+                .ForMember(x => x.WarehouseProduct,
+                    x => x.MapFrom(y =>
+                        new WarehouseProduct(y.WarehouseId != Guid.Empty ? y.WarehouseId : Guid.NewGuid(), y.Sku, y.Stock, y.Weight)));
+
         }
     }
 }
