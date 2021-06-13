4f499b15a14135624776f8259a5e2ad8c5b8f080
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-13 20:40:48 +0200

    Add ProductNotFoundException

---
+++ src/ProductManager/ProductManager.Core/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductManager.Core.Exceptions
+{
+    public class ProductNotFoundException : DomainException
+    {
+        public ProductNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
