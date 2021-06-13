af1e7a5df4b0066d4ab17da409bb604a36100430
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-13 19:20:11 +0200

    Add tests for domain models

---
+++ src/ProductManager/ProductManager.UnitTests/CatalogProductTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Exceptions;
+using Xunit;
+
+namespace ProductManager.UnitTests
+{
+    public class CatalogProductTests
+    {
+        [Theory]
+        [InlineData("123", "product name", "Placeholder product description")]
+        public void CreatingNewCatalogProduct_WithValidProperties_ShouldNotThrowAnyExceptions(string sku, string name, string description)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var catalogProduct = new CatalogProduct(Guid.NewGuid(), sku, name, description);
+                catalogProduct = new CatalogProduct(sku, name, description);
+            };
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("", "product name", "Placeholder product description")]
+        [InlineData(" ", "product name", "Placeholder product description")]
+        [InlineData("#$", "product name", "Placeholder product description")]
+        public void CreatingNewCatalogProduct_WithInvalidSku_ThrowsInvalidSkuException(string sku, string name, string description)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var catalogProduct = new CatalogProduct(sku, name, description);
+            };
+
+            // Assert
+            action.Should().Throw<InvalidSkuException>();
+        }
+
+        [Theory]
+        [InlineData("123", "", "Placeholder product description")]
+        [InlineData("123", " ", "Placeholder product description")]
+        [InlineData("123", "#$", "Placeholder product description")]
+        public void CreatingNewCatalogProduct_WithInvalidName_ThrowsInvalidProductNameException(string sku, string name, string description)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var catalogProduct = new CatalogProduct(sku, name, description);
+            };
+
+            // Assert
+            action.Should().Throw<InvalidProductNameException>();
+        }
+
+        [Theory]
+        [InlineData("123", "product name", "")]
+        [InlineData("123", "product name", " ")]
+        [InlineData("123", "product name", "#$")]
+        public void CreatingNewCatalogProduct_WithInvalidDescription_ThrowsInvalidDescriptionException(string sku, string name, string description)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var catalogProduct = new CatalogProduct(sku, name, description);
+            };
+
+            // Assert
+            action.Should().Throw<InvalidDescriptionException>();
+        }
+
+        [Fact]
+        public void ChangingCatalogProductSku_WithValidSku_ShouldUpdateSkuProperty()
+        {
+            // Arrange
+            var catalogProduct = new CatalogProduct("123", "product name", "Placeholder product description");
+            var newSku = "456";
+
+            // Act
+            catalogProduct.SetSku(newSku);
+
+            // Arrange
+            catalogProduct.Sku.Should().Be(newSku);
+        }
+
+        [Fact]
+        public void ChangingCatalogProductName_WithValidName_ShouldUpdateNameProperty()
+        {
+            // Arrange
+            var catalogProduct = new CatalogProduct("123", "product name", "Placeholder product description");
+            var newName = "new product name";
+
+            // Act
+            catalogProduct.SetName(newName);
+
+            // Arrange
+            catalogProduct.Name.Should().Be(newName);
+        }
+
+        [Fact]
+        public void ChangingCatalogProductDescription_WithValidDescription_ShouldUpdateDescriptionProperty()
+        {
+            // Arrange
+            var catalogProduct = new CatalogProduct("123", "product name", "Placeholder product description");
+            var newDescription = "New product description";
+
+            // Act
+            catalogProduct.SetDescription(newDescription);
+
+            // Arrange
+            catalogProduct.Description.Should().Be(newDescription);
+        }
+    }
+}
---
+++ src/ProductManager/ProductManager.UnitTests/WarehouseProductTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Exceptions;
+using Xunit;
+
+namespace ProductManager.UnitTests
+{
+    public class WarehouseProductTests
+    {
+        [Theory]
+        [InlineData("123", 12, 4.5)]
+        public void CreatingNewWarehouseProduct_WithValidProperties_ShouldNotThrowAnyExceptions(string sku, int stock, double weight)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var warehouseProduct = new WarehouseProduct(Guid.NewGuid(), sku, stock, weight);
+                warehouseProduct = new WarehouseProduct(sku, stock, weight);
+            };
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("", 12, 4.5)]
+        [InlineData("#$", 12, 4.5)]
+        public void CreatingNewWarehouseProduct_WithInvalidSku_ThrowsInvalidSkuException(string sku, int stock, double weight)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var warehouseProduct = new WarehouseProduct(sku, stock, weight);
+            };
+
+            // Assert
+            action.Should().Throw<InvalidSkuException>();
+        }
+
+        [Theory]
+        [InlineData("123", -1, 4.5)]
+        public void CreatingNewWarehouseProduct_WithInvalidStock_ThrowsInvalidStockException(string sku, int stock, double weight)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var warehouseProduct = new WarehouseProduct(sku, stock, weight);
+            };
+
+            // Assert
+            action.Should().Throw<InvalidStockException>();
+        }
+
+        [Theory]
+        [InlineData("123", 1, -1)]
+        [InlineData("123", 1, -0.1)]
+        public void CreatingNewWarehouseProduct_WithInvalidWeight_ThrowsInvalidWeightException(string sku, int stock, double weight)
+        {
+            // Arrange
+            Action action = () =>
+            {
+                // Act
+                var warehouseProduct = new WarehouseProduct(sku, stock, weight);
+            };
+
+            // Assert
+            action.Should().Throw<InvalidWeightException>();
+        }
+
+        [Fact]
+        public void ChangingWarehouseProductSku_WithValidSku_ShouldUpdateSkuProperty()
+        {
+            // Arrange
+            var warehouseProduct = new WarehouseProduct("123", 10, 2.2);
+            var newSku = "456";
+
+            // Act
+            warehouseProduct.SetSku(newSku);
+
+            // Arrange
+            warehouseProduct.Sku.Should().Be(newSku);
+        }
+
+        [Fact]
+        public void ChangingWarehouseProductStock_WithValidStock_ShouldUpdateStockProperty()
+        {
+            // Arrange
+            var warehouseProduct = new WarehouseProduct("123", 10, 2.2);
+            var newStock = 15;
+
+            // Act
+            warehouseProduct.SetStock(newStock);
+
+            // Arrange
+            warehouseProduct.Stock.Should().Be(newStock);
+        }
+
+        [Fact]
+        public void ChangingWarehouseProductWeight_WithValidWeight_ShouldUpdateWeightProperty()
+        {
+            // Arrange
+            var warehouseProduct = new WarehouseProduct("123", 10, 2.2);
+            var newWeight = 5;
+
+            // Act
+            warehouseProduct.SetWeight(newWeight);
+
+            // Arrange
+            warehouseProduct.Weight.Should().Be(newWeight);
+        }
+    }
+}
