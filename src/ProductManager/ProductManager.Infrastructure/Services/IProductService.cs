5dd45018e18983ef342d4392b71b9a94289b41ce
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-13 23:28:44 +0200

    Add IProductService with implementation and tests

---
+++ src/ProductManager/ProductManager.Infrastructure/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Infrastructure.DTO;
+
+namespace ProductManager.Infrastructure.Services
+{
+    public interface IProductService
+    {
+        Task AddAsync(ProductDto product);
+        Task<ProductDto> GetAsync(string sku);
+        Task DeleteAsync(string sku);
+        Task UpdateWarehouseAsync(WarehouseProductDto warehouseProductDto);
+        Task UpdateCatalogAsync(CatalogProductDto catalogProductDto);
+        Task UpdateSalesAsync(SalesProductDto salesProductDto);
+    }
+}
