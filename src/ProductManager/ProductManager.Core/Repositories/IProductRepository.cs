61cc9989d85137f8b45ae0b0305db9f712404a6e
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-13 20:41:03 +0200

    Add IProductRepository with InMemory implementation

---
+++ src/ProductManager/ProductManager.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Core.Domain;
+
+namespace ProductManager.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductAsync(string sku);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product updatedProduct);
+        Task DeleteAsync(string sku);
+    }
+}
---
+++ src/ProductManager/ProductManager.Infrastructure/Repositories/InMemoryProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Exceptions;
+using ProductManager.Core.Repositories;
+
+namespace ProductManager.Infrastructure.Repositories
+{
+    public class InMemoryProductRepository : IProductRepository
+    {
+        private readonly IMapper _mapper;
+        private readonly ISet<Product> _products = new HashSet<Product>();
+
+        public InMemoryProductRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<Product> GetProductAsync(string sku) =>
+            await Task.FromResult(_products.SingleOrDefault(x => x.CatalogProduct.Sku == sku));
+
+        public Task AddAsync(Product product)
+        {
+            _products.Add(product);
+            return Task.CompletedTask;
+        }
+
+        public async Task UpdateAsync(Product updatedProduct)
+        {
+            var product = await GetProductAsync(updatedProduct.CatalogProduct.Sku);
+            if (product == null)
+            {
+                throw new ProductNotFoundException($"Product with sku {updatedProduct.CatalogProduct.Sku} was not found!");
+            }
+
+            var updatedSales = _mapper.Map(updatedProduct.SalesProduct, product.SalesProduct);
+            var newProduct = _mapper.Map<Product, Product>(updatedProduct, product);
+        }
+
+        public async Task DeleteAsync(string sku)
+        {
+            var product = await GetProductAsync(sku);
+            if (product == null)
+            {
+                throw new ProductNotFoundException($"Product with sku {sku} was not found!");
+            }
+
+            _products.Remove(product);
+        }
+    }
+}
