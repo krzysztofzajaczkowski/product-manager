298c0df801672863a70b1688c86f330ab4d3cdb0
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-14 19:31:13 +0200

    Register AutoMapper and Product services in InfrastructureContainer

--- src/ProductManager/ProductManager.Infrastructure/IoC/InfrastructureContainer.cs
+++ src/ProductManager/ProductManager.Infrastructure/IoC/InfrastructureContainer.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using ProductManager.Core.Repositories;
+using ProductManager.Infrastructure.Configuration;
 using ProductManager.Infrastructure.Repositories;
 using ProductManager.Infrastructure.Services;
 using ProductManager.Infrastructure.Settings;
@@ -18,9 +19,12 @@ namespace ProductManager.Infrastructure.IoC
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
         {
+            services.AddAutoMapper(typeof(AutoMapperProfile));
             services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddSingleton<IProductRepository, InMemoryProductRepository>();
             services.AddTransient<IJwtHandler, JwtHandler>();
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IProductService, ProductService>();
 
             return services;
         }
