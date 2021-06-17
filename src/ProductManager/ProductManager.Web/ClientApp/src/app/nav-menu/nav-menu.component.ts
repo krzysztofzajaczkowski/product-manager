452c9fd75652a2282f8dabb4320befa8a4c887fa
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-17 22:56:14 +0200

    Update NavMenuComponent

--- src/ProductManager/ProductManager.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/ProductManager/ProductManager.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -8,11 +9,23 @@ import { Component } from '@angular/core';
 export class NavMenuComponent {
   isExpanded = false;
 
+  constructor(
+    private authService: AuthService
+  ) {}
+
   collapse() {
     this.isExpanded = false;
   }
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  get role() {
+    return localStorage.getItem("user_role");
+  }
+
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
   }
 }
