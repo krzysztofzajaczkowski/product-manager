214f416d8fc84f34c6c99e7e6d2974f4e145a724
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-17 19:31:38 +0200

    Add ProductService

---
+++ src/ProductManager/ProductManager.Web/ClientApp/src/app/product.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CreateProductDto } from './models/createProductDto';
+import { ProductBlockDto } from './models/productBlockDto';
+import { ProductDto } from './models/productDto';
+import { UpdateCatalogProductDto } from './models/updateCatalogProductDto';
+import { UpdateSalesProductDto } from './models/UpdateSalesProductDto';
+import { UpdateWarehouseProductDto } from './models/UpdateWarehouseProductDto';
+
+export const BASE_URL = new InjectionToken<string>('BASE_URL');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(
+    private httpClient: HttpClient,
+    @Inject(BASE_URL) private baseUrl: string
+  ) { }
+
+  browse(): Observable<any> {
+    return this.httpClient.get<ProductBlockDto[]>(this.baseUrl + 'products/browse');
+  }
+
+  get(sku: string): Observable<any> {
+    return this.httpClient.get<ProductDto>(this.baseUrl + `products/browse/${sku}`);
+  }
+
+  create(name: string, sku: string, description: string) : Observable<any> {
+    var createProductDto: CreateProductDto = {
+      name: name,
+      sku: sku,
+      description: description
+    };
+    return this.httpClient.post(this.baseUrl + 'products/create', createProductDto);
+  }
+
+  updateCatalog(id: string, name: string, sku: string, description: string) : Observable<any> {
+    var updateProductCatalogDto: UpdateCatalogProductDto = {
+      id: id,
+      name: name,
+      sku: sku,
+      description: description
+    };
+    return this.httpClient.post(this.baseUrl + 'products/update/catalog', updateProductCatalogDto);
+  }
+
+  updateWarehouse(id: string, sku: string, stock: number, weight: number) : Observable<any> {
+    var updateProductWarehouseDto: UpdateWarehouseProductDto = {
+      id: id,
+      sku: sku,
+      stock: stock,
+      weight: weight
+    };
+    return this.httpClient.post(this.baseUrl + 'products/update/warehouse', updateProductWarehouseDto);
+  }
+
+  updateSales(id: string, sku: string, cost: number, netPrice: number, taxPercentage: number) : Observable<any> {
+    var updateProductSalesDto: UpdateSalesProductDto = {
+      id: id,
+      sku: sku,
+      cost: cost,
+      netPrice: netPrice,
+      taxPercentage: taxPercentage
+    };
+    return this.httpClient.post(this.baseUrl + 'products/update/sales', updateProductSalesDto);
+  }
+}
