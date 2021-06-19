b45f805d636a1a429d83f168e65afd383218cdc0
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-19 20:08:07 +0200

    Add SQLiteConnectionFactoryTests

---
+++ src/ProductManager/ProductManager.Infrastructure/Database/SQLiteConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Infrastructure.Database
+{
+    public class SQLiteConnectionFactory : IDbConnectionFactory
+    {
+        public async Task<IDbConnection> CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ProductManager/ProductManager.UnitTests/SQLiteConnectionFactoryTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ProductManager.Infrastructure.Database;
+using Xunit;
+
+namespace ProductManager.UnitTests
+{
+    public class SQLiteConnectionFactoryTests : IDisposable
+    {
+        private readonly string _connectionString;
+        private readonly string _dbFileName;
+
+        public SQLiteConnectionFactoryTests()
+        {
+            _dbFileName = "file.db";
+            _connectionString = $"DataSource={_dbFileName}";
+        }
+
+        [Fact]
+        public void WhenCreatingSQLiteConnectionFactory_ConnectionStringFieldShouldBeEqualToPassedParameter()
+        {
+            var testConnectionString = "testConnString";
+            var sut = new SQLiteConnectionFactory(testConnectionString);
+
+            var connectionStringField = sut.GetType()
+                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance).First(x => x.Name == "_connectionString" && x.IsPrivate);
+
+            var value = (string) connectionStringField.GetValue(sut);
+
+            value.Should().Be(testConnectionString);
+        }
+
+        [Fact]
+        public async Task WhenCallingCreate_FactoryShouldReturnOpenedIDbConnection()
+        {
+            var sut = new SQLiteConnectionFactory(_connectionString);
+
+            var conn = await sut.CreateAsync();
+
+            var caughtState = conn.State;
+
+            conn.Close();
+
+            caughtState.Should().Be(ConnectionState.Open);
+        }
+
+        [Fact]
+        public async Task WhenCallingCreate_FactoryShouldCreateFileWithSpecifiedPath()
+        {
+            var sut = new SQLiteConnectionFactory(_connectionString);
+
+            var conn = await sut.CreateAsync();
+
+            conn.Close();
+
+            File.Exists(_dbFileName).Should().BeTrue();
+        }
+
+
+        public void Dispose()
+        {
+            if (File.Exists(_dbFileName))
+            {
+                File.Delete(_dbFileName);
+            }
+        }
+    }
+}
