ed1901e92c16956c01630b79bd6b9734ad56cc18
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-19 21:27:10 +0200

    Add PasswordHelper with unit tests

---
+++ src/ProductManager/ProductManager.Infrastructure/Helper/PasswordHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace ProductManager.Infrastructure.Helper
+{
+    public class PasswordHelper
+    {
+        public static string CalculateHash(string input)
+        {
+            var salt = GenerateSalt(16);
+
+            var bytes = KeyDerivation.Pbkdf2(input, salt, KeyDerivationPrf.HMACSHA512, 10000, 16);
+
+            return $"{ Convert.ToBase64String(salt) }:{ Convert.ToBase64String(bytes) }";
+        }
+
+        private static byte[] GenerateSalt(int length)
+        {
+            var salt = new byte[length];
+
+            using (var random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(salt);
+            }
+
+            return salt;
+        }
+
+        public static bool CheckMatch(string hash, string input)
+        {
+            try
+            {
+                var parts = hash.Split(':');
+
+                var salt = Convert.FromBase64String(parts[0]);
+
+                var bytes = KeyDerivation.Pbkdf2(input, salt, KeyDerivationPrf.HMACSHA512, 10000, 16);
+
+                return parts[1].Equals(Convert.ToBase64String(bytes));
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
