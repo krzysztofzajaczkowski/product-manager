2a9762e78d5850b57a52c611ca19816b5831f08b
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-19 21:11:57 +0200

    Add SQLiteUserRepository with integration tests

---
+++ src/ProductManager/ProductManager.Infrastructure/Repositories/SQLiteUserRepository.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Repositories;
+using ProductManager.Infrastructure.Database;
+
+namespace ProductManager.Infrastructure.Repositories
+{
+    public class SQLiteUserRepository : IUserRepository
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public SQLiteUserRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+        public async Task<User> GetUserAsync(Guid id)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            var user = (await connection.QueryAsync<User, Role, User>(
+                    @"SELECT * FROM Users users
+                        INNER JOIN UserRoles userRoles ON users.Id = userRoles.UserId
+                        INNER JOIN Roles roles ON userRoles.RoleId = roles.Id
+                        WHERE users.Id = @Id", (user, role) =>
+                    {
+                        user.AddRole(role);
+                        return user;
+                    },
+                    new
+                    {
+                        Id = id
+                    })).GroupBy(u => u.Id)
+                .Select(g =>
+                {
+                    var combinedUser = g.First();
+                    var roles = g.Skip(1).Select(u => u.Roles.Single());
+                    foreach (var role in roles)
+                    {
+                        combinedUser.AddRole(role);
+                    }
+
+                    return combinedUser;
+                }).First();
+
+            return user;
+        }
+
+        public async Task<User> GetUserAsync(string email)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            var user = (await connection.QueryAsync<User, Role, User>(
+                    @"SELECT * FROM Users users
+                        INNER JOIN UserRoles userRoles ON users.Id = userRoles.UserId
+                        INNER JOIN Roles roles ON userRoles.RoleId = roles.Id
+                        WHERE Email = @Email", (user, role) =>
+                    {
+                        user.AddRole(role);
+                        return user;
+                    },
+                    new
+                    {
+                        Email = email
+                    })).GroupBy(u => u.Id)
+                .Select(g =>
+                {
+                    var combinedUser = g.First();
+                    var roles = g.Skip(1).Select(u => u.Roles.Single());
+                    foreach (var role in roles)
+                    {
+                        combinedUser.AddRole(role);
+                    }
+
+                    return combinedUser;
+                }).First();
+
+
+            return user;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            await connection.ExecuteAsync("INSERT INTO Users(Id, Email, Name, Password) VALUES (@Id, @Email, @Name, @Password);",
+                user);
+
+            foreach (var userRole in user.Roles)
+            {
+                var role = await GetRoleAsync(userRole.Name);
+
+                if (role == null)
+                {
+                    //await connection.ExecuteAsync("INSERT INTO Roles(Id, Name) VALUES (@id, @name);", userRole);
+                    await AddAsync(userRole);
+                    role = userRole;
+                }
+
+                await connection.ExecuteAsync("INSERT INTO UserRoles(UserId, RoleId) VALUES(@UserId, @RoleId);", new
+                {
+                    UserId = user.Id,
+                    RoleId = role.Id
+                });
+            }
+        }
+
+        public async Task<Role> GetRoleAsync(Guid id)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            var role = await connection.QuerySingleOrDefaultAsync<Role>("SELECT * FROM Roles WHERE Id = @Id;",
+                new { Id = id });
+            return role;
+        }
+
+        public async Task<Role> GetRoleAsync(string name)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            var role = await connection.QuerySingleOrDefaultAsync<Role>("SELECT * FROM Roles WHERE Name = @Name;",
+                new { Name = name });
+            return role;
+        }
+
+        public async Task AddAsync(Role role)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+            await connection.ExecuteAsync("INSERT INTO Roles(Id, Name) VALUES (@Id, @Name);", role);
+        }
+    }
+}
---
+++ src/ProductManager/ProductManager.IntegrationTests/SQLiteUserRepositoryTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using ProductManager.Infrastructure.Database;
+using ProductManager.Infrastructure.Repositories;
+using ProductManager.Infrastructure.Services;
+using ProductManager.Infrastructure.Settings;
+using Xunit;
+
+namespace ProductManager.IntegrationTests
+{
+    public class SQLiteUserRepositoryTests : IDisposable
+    {
+        private readonly string _dbFileName;
+        private readonly string _connectionString;
+
+        public SQLiteUserRepositoryTests()
+        {
+            _dbFileName = "database.db";
+            _connectionString = $"DataSource={_dbFileName};BinaryGUID=False;";
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_AdminAccountShouldExist()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var account = await sut.GetUserAsync("admin@admin.com");
+
+            // Assert
+            account.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_CatalogManagerRoleShouldExist()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var role = await sut.GetRoleAsync("CatalogManager");
+
+            // Assert
+            role.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_WarehouseManagerRoleShouldExist()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var role = await sut.GetRoleAsync("WarehouseManager");
+
+            // Assert
+            role.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_SalesManagerRoleShouldExist()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var role = await sut.GetRoleAsync("SalesManager");
+
+            // Assert
+            role.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_AdminAccountShouldHaveCatalogManagerRole()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var account = await sut.GetUserAsync("admin@admin.com");
+
+            // Assert
+            account.Roles.Should().Contain(r => r.Name == "CatalogManager");
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_AdminAccountShouldHaveWarehouseManagerRole()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var account = await sut.GetUserAsync("admin@admin.com");
+
+            // Assert
+            account.Roles.Should().Contain(r => r.Name == "WarehouseManager");
+        }
+
+        [Fact]
+        public async Task WhenUsingDatabaseInitializer_AdminAccountShouldHaveSalesManagerRole()
+        {
+            // Arrange
+            var dbConnectionFactory = new SQLiteConnectionFactory(_connectionString);
+            var dbInitializer = new DatabaseInitializier(dbConnectionFactory);
+            var sut = new SQLiteUserRepository(dbConnectionFactory);
+
+            await dbInitializer.SeedDatabaseAsync();
+
+            // Act
+            var account = await sut.GetUserAsync("admin@admin.com");
+
+            // Assert
+            account.Roles.Should().Contain(r => r.Name == "SalesManager");
+        }
+
+        public void Dispose()
+        {
+            if (File.Exists(_dbFileName))
+            {
+                File.Delete(_dbFileName);
+            }
+        }
+    }
+}
