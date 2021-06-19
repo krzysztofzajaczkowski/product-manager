07ac52eb11c33cef6a7492b1d34ad97d9fa4075d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-19 22:38:52 +0200

    Add SQLiteProductRepository with tests

---
+++ src/ProductManager/ProductManager.Infrastructure/Repositories/SQLiteProductRepository.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using ProductManager.Core.Domain;
+using ProductManager.Core.Repositories;
+using ProductManager.Infrastructure.Database;
+
+namespace ProductManager.Infrastructure.Repositories
+{
+    public class SQLiteProductRepository : IProductRepository
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public SQLiteProductRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<Product> GetProductAsync(string sku)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            var product = (await connection.QueryAsync(
+                @"SELECT 
+                        cp.Id as CatalogId,
+                        wp.Id as WarehouseId,
+                        sp.Id as SalesId,
+                        cp.Sku,
+                        Name,
+                        Description,
+                        Stock,
+                        Weight,
+                        Cost,
+                        TaxPercentage,
+                        NetPrice
+                    FROM CatalogProducts cp
+                    INNER JOIN WarehouseProducts wp ON cp.Sku = wp.Sku
+                    INNER JOIN SalesProducts sp ON cp.Sku = sp.Sku
+                    WHERE cp.Sku = @Sku;", new
+                {
+                    Sku = sku
+                })).Select(row => new Product(Guid.Parse(row.CatalogId), (string)row.Sku, (string)row.Name, (string)row.Description, Guid.Parse(row.WarehouseId),
+                (int)row.Stock, (double)row.Weight, Guid.Parse(row.SalesId), (decimal)row.Cost, (int)row.TaxPercentage, (decimal)row.NetPrice)).FirstOrDefault();
+
+            return product;
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            await connection.ExecuteAsync(
+                "INSERT INTO CatalogProducts(Id, Sku, Name, Description) VALUES (@Id, @Sku, @Name, @Description);",
+                product.CatalogProduct);
+
+            await connection.ExecuteAsync(
+                "INSERT INTO WarehouseProducts(Id, Sku, Stock, Weight) VALUES (@Id, @Sku, @Stock, @Weight);",
+                product.WarehouseProduct);
+
+            await connection.ExecuteAsync(
+                "INSERT INTO SalesProducts(Id, Sku, Cost, TaxPercentage, NetPrice) VALUES (@Id, @Sku, @Cost, @TaxPercentage, @NetPrice);",
+                product.SalesProduct);
+        }
+
+        public async Task UpdateAsync(Product updatedProduct)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            await connection.ExecuteAsync(@"
+                UPDATE CatalogProducts
+                SET
+                    Name = @Name,
+                    Description = @Description
+                WHERE
+                    Sku = @Sku", updatedProduct.CatalogProduct);
+
+            await connection.ExecuteAsync(@"
+                UPDATE WarehouseProducts
+                SET
+                    Stock = @Stock,
+                    Weight = @Weight
+                WHERE
+                    Sku = @Sku", updatedProduct.WarehouseProduct);
+
+            await connection.ExecuteAsync(@"
+                UPDATE SalesProducts
+                SET
+                    Cost = @Cost,
+                    TaxPercentage = @TaxPercentage,
+                    NetPrice = @NetPrice
+                WHERE
+                    Sku = @Sku", updatedProduct.SalesProduct);
+
+        }
+
+        public async Task DeleteAsync(string sku)
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            await connection.ExecuteAsync("DELETE FROM CatalogProducts WHERE Sku = @Sku;", new
+            {
+                Sku = sku
+            });
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            using var connection = await _dbConnectionFactory.CreateAsync();
+
+            var products = (await connection.QueryAsync(
+                @"SELECT 
+                        cp.Id as CatalogId,
+                        wp.Id as WarehouseId,
+                        sp.Id as SalesId,
+                        cp.Sku,
+                        Name,
+                        Description,
+                        Stock,
+                        Weight,
+                        Cost,
+                        TaxPercentage,
+                        NetPrice
+                    FROM CatalogProducts cp
+                    INNER JOIN WarehouseProducts wp ON cp.Sku = wp.Sku
+                    INNER JOIN SalesProducts sp ON cp.Sku = sp.Sku;")).Select(row => new Product(Guid.Parse(row.CatalogId), (string)row.Sku, (string)row.Name, (string)row.Description, Guid.Parse(row.WarehouseId),
+                (int)row.Stock, (double)row.Weight, Guid.Parse(row.SalesId), (decimal)row.Cost, (int)row.TaxPercentage, (decimal)row.NetPrice)).ToList();
+
+            return products;
+        }
+    }
+}
