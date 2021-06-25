237cc33252900f86ea8f305dd968492c05b8938c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-25 21:32:28 +0200

    Add ValueObjects for domain classes

---
+++ src/ProductManager/ProductManager.Core/Domain/ValueObjects/ProductName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ProductManager.Core.Exceptions;
+
+namespace ProductManager.Core.Domain.ValueObjects
+{
+    public class ProductName : ValueObject
+    {
+        private readonly Regex _nameRegex = new Regex(@"^[a-zA-Z0-9 ]+$");
+        public string Name { get; protected set; }
+
+        public ProductName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name) || !_nameRegex.IsMatch(name))
+            {
+                throw new InvalidProductNameException("Product name should contain only alphanumeric characters and spaces.");
+            }
+            Name = name;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Name;
+        }
+    }
+}
---
+++ src/ProductManager/ProductManager.Core/Domain/ValueObjects/StockKeepingUnit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ProductManager.Core.Exceptions;
+
+namespace ProductManager.Core.Domain.ValueObjects
+{
+    public class StockKeepingUnit : ValueObject
+    {
+        private readonly Regex _skuRegex = new Regex(@"^[a-zA-Z0-9]+$");
+        public string Sku { get; protected set; }
+
+        public StockKeepingUnit(string sku)
+        {
+            if (string.IsNullOrWhiteSpace(sku) || !_skuRegex.IsMatch(sku))
+            {
+                throw new InvalidSkuException("Product sku should contain only alphanumeric characters.");
+            }
+            Sku = sku;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Sku;
+        }
+    }
+}
