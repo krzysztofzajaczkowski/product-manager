cb671b2bd8007cb78df9a993c1245f9be3d4da8b
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-06-26 09:15:41 +0200

    Add HttpClientFixture with collection definition

---
+++ src/ProductManager/ProductManager.AcceptanceTests/HttpClientFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Ductus.FluentDocker.Commands;
+
+namespace ProductManager.AcceptanceTests
+{
+    public class HttpClientFixture : IDisposable
+    {
+        public HttpClient Client { get; set; }
+
+        public HttpClientFixture()
+        {
+            Client = new HttpClient();
+        }
+
+
+        public void Dispose()
+        {
+            Client?.Dispose();
+        }
+    }
+}
